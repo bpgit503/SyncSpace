a092bb056361c319987d8e90c4e866a3bb144ef8
Brenden Pulliam <bpgit503@gmail.com>
2025-09-09 12:21:21 +0200

    Created Trainer Entity Class

---
+++ src/main/java/com/devbp/syncspace/domain/entities/Trainers.java
@@ -0,0 +1,47 @@
+package com.devbp.syncspace.domain.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.CollectionId;
+
+@Entity
+@Table(name = "trainers")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Trainers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull(message = "User id is required")
+    @Column(name = "user_id")
+    private long userId;
+
+    @Column(columnDefinition = "TEXT")
+    private String specializations;
+
+    @Column(columnDefinition = "TEXT")
+    private String certifications;
+
+    @Column(name = "contract_details",columnDefinition = "TEXT")
+    private String contractDetails;
+
+    @NotNull(message = "Earning Percentage is required")
+    @Column(name = "earnings_percentage")
+    private double earningsPercentage;
+
+    @Column(columnDefinition = "TEXT")
+    private String bio;
+
+    private int experienceYears;
+
+    @Column(name = "is_available")
+    private boolean isAvailable;
+
+
+}
