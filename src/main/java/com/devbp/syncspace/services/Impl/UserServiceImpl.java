b900440bc5f8f1c3f4c118db9bd3151bb752e153
Brenden Pulliam <bpgit503@gmail.com>
2025-09-09 12:54:43 +0200

    refactored get all user method in user service

--- src/main/java/com/devbp/syncspace/services/Impl/UserServiceImpl.java
+++ src/main/java/com/devbp/syncspace/services/Impl/UserServiceImpl.java
@@ -1,14 +1,22 @@
 package com.devbp.syncspace.services.Impl;
 
 import com.devbp.syncspace.domain.entities.User;
+import com.devbp.syncspace.repositories.UserRepository;
 import com.devbp.syncspace.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@AllArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
     @Override
     public List<User> getAllUsers() {
-        return List.of();
+        return userRepository.findAll();
     }
 
     @Override
