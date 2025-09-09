7ca1c177f5baf52cc3ddbc35713e01c2b3de26a0
Brenden Pulliam <bpgit503@gmail.com>
2025-09-09 15:25:25 +0200

    Created UserMapper class to map entity to Dto

--- src/main/java/com/devbp/syncspace/controllers/UserController.java
+++ src/main/java/com/devbp/syncspace/controllers/UserController.java
@@ -1,9 +1,15 @@
 package com.devbp.syncspace.controllers;
 
+import com.devbp.syncspace.domain.dtos.UserDto;
 import com.devbp.syncspace.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.digester.ArrayStack;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("ap1/v1/user")
@@ -12,5 +18,9 @@ public class UserController {
 
     private final UserService userService;
 
-
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        List<UserDto> dummy = new ArrayList<>();
+        //TODO
+        return ResponseEntity.ok(dummy);
+    }
 }
