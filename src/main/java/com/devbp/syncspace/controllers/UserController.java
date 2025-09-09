d8ba39aaa315c4600aa7da90830a934ade3fc0e9
Brenden Pulliam <bpgit503@gmail.com>
2025-09-09 15:34:49 +0200

    refactored get all users method in user controller and incorporated userdto and mapper

--- src/main/java/com/devbp/syncspace/controllers/UserController.java
+++ src/main/java/com/devbp/syncspace/controllers/UserController.java
@@ -1,26 +1,30 @@
 package com.devbp.syncspace.controllers;
 
 import com.devbp.syncspace.domain.dtos.UserDto;
+import com.devbp.syncspace.domain.mappers.UserMapper;
 import com.devbp.syncspace.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.digester.ArrayStack;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("ap1/v1/user")
+@RequestMapping("api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+    private final UserMapper userMapper;
 
-    public ResponseEntity<List<UserDto>> getAllUsers(){
-        List<UserDto> dummy = new ArrayList<>();
-        //TODO
-        return ResponseEntity.ok(dummy);
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        List<UserDto> userDtos = userService.getAllUsers().stream()
+                .map(userMapper::toDto)
+                .toList();
+
+        return ResponseEntity.ok(userDtos);
     }
 }
