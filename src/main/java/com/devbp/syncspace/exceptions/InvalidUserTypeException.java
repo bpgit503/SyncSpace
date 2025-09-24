e30898f4297bd6a3e204a036f78adfab463a9981
Brenden Pulliam <bpgit503@gmail.com>
2025-09-24 17:30:57 +0200

    Created Invalid User Type Exception class

---
+++ src/main/java/com/devbp/syncspace/exceptions/InvalidUserTypeException.java
@@ -0,0 +1,7 @@
+package com.devbp.syncspace.exceptions;
+
+public class InvalidUserTypeException extends RuntimeException {
+    public InvalidUserTypeException(String message) {
+        super(message);
+    }
+}
