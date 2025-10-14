7c3abd1a750c88567e01acdc3fabedb38dc21997
Brenden Pulliam <bpgit503@gmail.com>
2025-10-14 18:10:25 +0200

    refactored Booking Response Dto and booking Mapper

--- src/main/java/com/devbp/syncspace/domain/dtos/BookingResponseDto.java
+++ src/main/java/com/devbp/syncspace/domain/dtos/BookingResponseDto.java
@@ -7,7 +7,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Data
 @AllArgsConstructor
@@ -16,11 +18,21 @@ import java.time.LocalDateTime;
 public class BookingResponseDto {
 
     private long id;
-    private UserResponseDto client;
-    private ClassResponseDto clazz;
     private LocalDateTime bookingDate;
     private BookingStatus bookingStatus;
     private double pricePaid;
     private PaymentStatus paymentStatus;
     private String notes;
+
+    private Long clientId;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    private long classId;
+    private LocalDate scheduledDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private int maxCapacity;
 }
--- src/main/java/com/devbp/syncspace/domain/mappers/BookingMapper.java
+++ src/main/java/com/devbp/syncspace/domain/mappers/BookingMapper.java
@@ -3,11 +3,22 @@ package com.devbp.syncspace.domain.mappers;
 import com.devbp.syncspace.domain.dtos.BookingResponseDto;
 import com.devbp.syncspace.domain.entities.Booking;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE,
 uses = {UserMapper.class, ClassMapper.class})
 public interface BookingMapper {
 
+    @Mapping(target = "clientId", source = "booking.client.id")
+    @Mapping(target = "email", source = "booking.client.email")
+    @Mapping(target = "firstName", source = "booking.client.firstName")
+    @Mapping(target = "lastName", source = "booking.client.lastName")
+    @Mapping(target = "phoneNumber", source = "booking.client.phoneNumber")
+    @Mapping(target = "classId", source = "booking.clazz.id")
+    @Mapping(target = "scheduledDate", source = "booking.clazz.scheduledDate")
+    @Mapping(target = "startTime", source = "booking.clazz.startTime")
+    @Mapping(target = "endTime", source = "booking.clazz.endTime")
+    @Mapping(target = "maxCapacity", source = "booking.clazz.maxCapacity")
     BookingResponseDto toDto(Booking booking);
 }
